77b842ae457d9e1f731f893a5a6f5fc597bc32bf
bantheus <matheuschmidt.b@gmail.com>
2022-04-25 14:20:46 -0300

    update: foreach pizzas

--- php/src/index.php
+++ php/src/index.php
@@ -40,15 +40,15 @@
     <div class="container">
         <div class="row">
 
-            <?php foreach($pizzas as $pizza){ ?>
+            <?php foreach($pizzas as $pizza): ?>
                 <div class="col s6 md3">
                     <div class="card z-depth-0">
                         <div class="card-content center">
                             <h6><?php echo htmlspecialchars($pizza['titulo']); ?></h6>
                             <ul>
-                                <?php foreach(explode(',', $pizza['ingredientes']) as $ingrediente){ ?>
+                                <?php foreach(explode(',', $pizza['ingredientes']) as $ingrediente): ?>
                                     <li><?php echo htmlspecialchars($ingrediente); ?></li>
-                                <?php } ?>
+                                <?php endforeach ?>
                             </ul>
                         </div>
                         <div class="card-action right-align">
@@ -56,7 +56,7 @@
                         </div>
                     </div>
                 </div>
-            <?php } ?>
+            <?php endforeach ?>
 
         </div>
     </div>
